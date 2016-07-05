804366bc4fc5bf0938706a129c7f19ba80d758b3
Marselliy <sariksariksarik@gmail.com>
2016-07-06 00:24:11 +0300

    Add func example

---
+++ examples/exampleFunc.js
@@ -0,0 +1,48 @@
+var categorization = require('../src/categorization');
+
+/**
+ * Initialize categorizer
+ * Alphabet : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+ * Intervals : equal
+ * Number of derivatives : 10
+ */
+categorizer = categorization.getCategorizer('0123456789'.split(''), 'EQUAL', 10);
+
+/**
+ * Train categorizer with linear function
+ */
+categorizer.train(
+[
+  {
+    data : [1,3,5,7,9,11,13,15,17,19,21,23,25],
+    name : 'y'
+  }
+], 'Linear');
+
+/**
+ * Train categorizer with quadratic function
+ */
+categorizer.train(
+[
+  {
+    data : [1,4,9,16,25,36,49,64,81,100,121,144,169],
+    name : 'y'
+  }
+], 'Quadratic');
+
+/**
+ * Train categorizer with expontial function
+ */
+categorizer.train(
+[
+  {
+    data : [1,2,4,8,16,32,64,128,256,512,1024,2048,4096],
+    name : 'y'
+  }
+], 'Expontial');
+
+/**
+ * Categorize unknown function
+ * Returns 'Linear'
+ */
+console.log(categorizer.categorize([{data : [-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18], name : 'y'}]).result);
